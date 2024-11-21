2ce5ec41a50bc73948156b6dae2c3eff1692b654
thecabrall <gruposppider@gmail.com>
2024-11-21 09:28:35 -0300

    v1.1

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import html_download, csv_enviado, download_modelo
 
 urlpatterns = [
-    path('', csv_enviado, name='upload_csv'),
+    path('https://obra-larmail.onrender.com', csv_enviado, name='upload_csv'),
     # path('process-csv', ext_data, name='process'),
     path('download/', html_download, name='download_html'),
     path('modelo-csv/', download_modelo, name='download_csv'),
--- setup/urls.py
+++ setup/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('produtos.urls'))
+    path('https://obra-larmail.onrender.com',include('produtos.urls'))
 ]
